7a324341b48e13aab1ffbe1b88ea90be2bc7d8f5
DavoDeveloper <davitdevelop@gmail.com>
2023-02-04 21:32:05 +0400

    refactoring users component to clas Users

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -2,50 +2,45 @@ import axios from "axios";
 import React from "react";
 import s from "./Users.module.css";
 
-const Users = (props) => {
-  if (props.users.length === 0) {
-    //   props.setusers([
-    //     { id: 1, name: "Simon", condition: "I am Boss", followed: true, location: { city: "Moscow", country: "Russsia" }, img: "https://img.freepik.com/free-photo/close-up-portrait-young-bearded-man-face_171337-2887.jpg?w=360" },
-    //     { id: 2, name: "Jack", condition: "I am Boss too", followed: false, location: { city: "Berlin", country: "Germany" }, img: "https://img.freepik.com/free-photo/red-haired-serious-young-man-blogger-looks-confidently_273609-16730.jpg?w=360" },
-    //     { id: 3, name: "John", condition: "I am Boss too", followed: false, location: { city: "Paris", country: "France" }, img: "https://img.freepik.com/free-photo/close-up-young-successful-man-smiling-camera-standing-casual-outfit-against-blue-background_1258-66609.jpg?w=2000" },
-    //   ]);
-    axios.get("https://reqres.in/api/users?page=2").then((response) => props.setUsers([response.data.data]));
+class Users extends React.Component {
+  componentDidMount() {
+    axios.get("https://reqres.in/api/users?page=2").then((response) => this.props.setUsers(response.data.data));
   }
-  let newUsers;
-  props.users.map((u) => {
-    newUsers = u.map((items) => {
-      console.log(items);
-      let styles = {
-        backgroundImage: `url('${items.avatar}')`,
-      };
-      return (
-        <div className={s.userBlock}>
-          <div className={s.userImg}>
-            <div style={styles}></div>
-            {u.followed ? (
-              <button onClick={() => props.unfollow(u.id)} className="btn btn-info mt-3 mb-3">
-                Unfollow
-              </button>
-            ) : (
-              <button onClick={() => props.follow(u.id)} className="btn btn-info mt-3 mb-3">
-                Follow
-              </button>
-            )}
-          </div>
-          <div className={s.personalInfo}>
-            <div>
-              <div className={s.name}>{items.first_name}</div>
-              <div className={s.condition}>{items.last_name}</div>
+
+  render() {
+    const styles = (src) => ({
+      backgroundImage: `url('${src.avatar}')`,
+    });
+    return (
+      <div>
+        {this.props.users.map((items) => (
+          <div className={s.userBlock}>
+            <div className={s.userImg}>
+              <div style={styles(items)}></div>
+              {items.followed ? (
+                <button onClick={() => this.props.unfollow(items.id)} className="btn btn-dark mt-3 mb-3">
+                  Unfollow
+                </button>
+              ) : (
+                <button onClick={() => this.props.follow(items.id)} className="btn btn-info mt-3 mb-3">
+                  Follow
+                </button>
+              )}
+            </div>
+            <div className={s.personalInfo}>
+              <div>
+                <div className={s.name}>{items.first_name}</div>
+                <div className={s.condition}>{items.last_name}</div>
+              </div>
+            </div>
+            <div className={s.email}>
+              <div>{items.email}</div>
             </div>
           </div>
-          <div className={s.email}>
-            <div>{items.email}</div>
-          </div>
-        </div>
-      );
-    });
-  });
-  return <div className="row">{newUsers}</div>;
-};
+        ))}
+      </div>
+    );
+  }
+}
 
 export default Users;
